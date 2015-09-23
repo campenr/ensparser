f55093eb97943edb1bbbed7e6962deb23dcda2b7
campenr <richard@campen.co>
2015-09-23 10:47:52 -0400

    Fixed test data directory variables to support python -m unittest discover calls.

--- multiplate/test/test.py
+++ multiplate/test/test.py
@@ -1,16 +1,20 @@
 import unittest
 
+import os.path
 import pandas as pd
 
 from multiplate import multiplateIO
 
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "test_data")
 
 class TestIsInstance(unittest.TestCase):
 
     def test_enspire_csv_parser(self):
         """Check that parsed EnSpire csv matches expected data"""
-        input_data = multiplateIO.parse_csv("test_data/EnSpire/raw_plate_data_enspire.csv", "enspire")
-        expected_data = pd.read_pickle("test_data/EnSpire/expected_plate_data.pkl")
+        test_dir = os.path.join(TEST_DATA_DIR, "EnSpire")
+        input_data = multiplateIO.parse_csv(os.path.join(test_dir, "raw_plate_data_enspire.csv"), "enspire")
+        expected_data = pd.read_pickle(os.path.join(test_dir, "expected_plate_data.pkl"))
+        # input_data is an iterator with one item
         for data_set in input_data:
             self.assertTrue(data_set.equals(expected_data))
 
